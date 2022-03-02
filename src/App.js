a23ab201682d4b74f99b22ff251163688acc8530
ThePhiri <ThePhiri@Gotbots-MacBook-Pro.local>
2022-03-02 13:14:57 +0200

    adding dark mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import ThemeContext from './context/ThemeContext';
 
 function App() {
-  return (
-    <Dashboard />
-  );
+  const [darkMode, setDarkMode] = useState(true);
+  return <ThemeContext.Provider value={{darkMode, setDarkMode}}> <Dashboard/></ThemeContext.Provider>
+
 }
 
 export default App;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,21 @@
-import React from "react"
+import React, { useContext } from "react"
 import { mockCompanyDetails } from "../constants/mock"
-import Card from "./Card"
+import Chart from "./Chart"
 import Details from "./Details"
 import Header from "./Header"
 import Overview from "./Overview"
+import ThemeContext from "../context/ThemeContext";
 
 
 
 const Dashboard = () => {
-return <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand bg-neutral-100">
+    const { darkMode } = useContext(ThemeContext);
+return <div className={`h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand  ${darkMode ? "bg-gray-900 text-gray-300" : "bg-neutral-100"}`}>
     <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
     <Header name={ mockCompanyDetails.name } />
     </div>
     <div className="md:col-span-2 row-span-4">
-        <Card>
-            Chart
-        </Card>
+        <Chart/>
     </div>
     <div>
         <Overview 
