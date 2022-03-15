111384232eb977a49558a5bccbc7e872bc6b4724
ThePhiri <ThePhiri@Gotbots-MacBook-Pro.local>
2022-03-15 03:59:28 +0200

    add final transitions

--- src/components/ChartFilter.js
+++ src/components/ChartFilter.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ChartFilter = ({ text, active, onClick }) => {
-    return <button onClick={onClick} className={`w-12 m-2 h-8 border-1 rounded-md flex items-center justify-center cursor ${active ? "bg-indigo-600 border-indigo-700 text-gray-100" : "border-indigo-300 text-indigo-300"}`}>
+    return <button onClick={onClick} className={`w-12 m-2 h-8 border-1 rounded-md flex items-center justify-center cursor ${active ? "bg-indigo-600 border-indigo-700 text-gray-100" : "border-indigo-300 text-indigo-300"} transition duration-200 hover:bg-indigo-600 hover:text-gray-100 hover:border-indigo-700`}>
         {text}
     </button>
 }
--- src/components/ThemeIcon.js
+++ src/components/ThemeIcon.js
@@ -9,7 +9,7 @@ const ThemeIcon = () => {
         setDarkMode(!darkMode);
     }
 
-    return <button className={`rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-32 shadow-lg ${darkMode ? "shadow-gray-800" : null}`} onClick={toggleDarkMode}>
+    return <button className={`rounded-lg border-1 border-neutral-400 p-2 absolute right-8 xl:right-32 shadow-lg ${darkMode ? "shadow-gray-800" : null} transtion duration-300 hover:scale-125`} onClick={toggleDarkMode}>
         <MoonIcon className={`h-8 w-8 cursor-pointer stroke-1  ${darkMode ? "fill-yellow-400 stroke-yellow-400" : "fill-none stroke-neutral-400" }`}/>
     </button>
 };
